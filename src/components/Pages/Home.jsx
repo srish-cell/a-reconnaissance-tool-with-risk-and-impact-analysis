106a84d67073276ede6b590b0499d8462618bca2
sridharan377 <sridharan3770@gmail.com>
2024-08-01 21:44:33 +0530

    commit it

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -5,13 +5,12 @@ const Home = () => {
   return (
     <div>
       <h1>Welcome to Recon-Tool</h1>
-      <h2>Hi Sridharan!!, Srish </h2>
+      <h2> about our tool </h2>
       <h1> </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      
+
+
+      <h1> why this tool</h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
